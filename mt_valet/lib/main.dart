3fc5f30bb9faba5f066dd64bbd0809ca5267c841
tamen <20100902@mail.wit.ie>
2025-02-08 22:34:12 +0000

    Update login screen

--- mt_valet/lib/main.dart
+++ mt_valet/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mt_valet/generate_qr_code_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mt_valet/screens/login.dart';
 import 'firebase_options.dart';
--- mt_valet/lib/screens/login.dart
+++ mt_valet/lib/screens/login.dart
@@ -1,13 +1,97 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-const LoginScreen({super.key});
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text("Login Screen"),
-    );
+        body: Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/logo.jpg', 
+            height: 120, 
+          ),
+          SizedBox(height: 16.0),
+          Text("Login", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+          SizedBox(
+            height: 24.0,
+          ),
+          Row(children: [
+            Text(
+              "Email",
+              textAlign: TextAlign.start,
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+          ]),
+          SizedBox(height: 4.0),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Enter your email address",
+              border: OutlineInputBorder(),
+            ),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Row(children: [
+            Text(
+              "Password",
+              textAlign: TextAlign.start,
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+          ]),
+          SizedBox(height: 4.0),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Enter your password",
+              border: OutlineInputBorder(),
+            ),
+            obscureText: true,
+          ),
+          SizedBox(height: 32.0),
+          SizedBox(
+            width: double.infinity,
+            height: 48,
+            child: ElevatedButton(
+              onPressed: () {
+                //login logic
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Color(0xFF054CE5)),
+                foregroundColor: WidgetStatePropertyAll(
+                  Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+              child: Text(
+                "Login",
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Don't have an account ? "),
+              Text(
+                "Register",
+                style: TextStyle(
+                    color: Color(0xFF054CE5),
+                    decoration: TextDecoration.underline,
+                    decorationColor: Color(0xFF054CE5)),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }

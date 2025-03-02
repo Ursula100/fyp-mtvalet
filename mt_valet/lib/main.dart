1e33f75f9744be14a2132d98625262a2a9a17507
tamen <20100902@mail.wit.ie>
2025-03-02 14:27:57 +0000

    Secure keys in keys in .env file - use flutter_env

--- mt_valet/lib/main.dart
+++ mt_valet/lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mt_valet/screens/login.dart';
-import 'package:mt_valet/screens/registration.dart';
+//import 'package:mt_valet/screens/registration.dart';
 import 'firebase_options.dart';
 
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
@@ -22,7 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: const Color.fromARGB(255, 5, 132, 236)),
-      home: const RegistrationScreen(),
+      home: const LoginScreen(),
     );
   }
 }
